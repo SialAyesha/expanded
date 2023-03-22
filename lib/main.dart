57b3b40b73684c1b6f28692b2ccbaa1835fcd4b8
18bsc <wizz.muaaz@gmail.com>
2023-03-22 11:37:19 +0500

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,62 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
       home: SafeArea(
         child: Scaffold(
           body: Column(
             children: [
-              Container(
-                height: 60,
-                width: 150,
-                decoration: BoxDecoration(
+              Expanded(
+                flex: 3,
+                child: Container(
+                  color: Colors.yellow,
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  width: 300,
+                  color: Colors.red,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  width: 150,
                   color: Colors.blue,
                 ),
-                child: Center(child: Text('by')),
               ),
-              Container(
-                height: 60,
-                width: 150,
-                decoration: BoxDecoration(
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.black,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  width: 150,
                   color: Colors.blue,
                 ),
-                child: Center(child: Text('by')),
               ),
-              Container(
-                height: 60,
-                width: 150,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
+              Expanded(
+                flex: 2,
+                child: Container(
+                  width: 300,
+                  color: Colors.red,
                 ),
-                child: Center(child: Text('by')),
               ),
-              Container(
-                height: 60,
-                width: 150,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
+              Expanded(
+                flex: 3,
+                child: Container(
+                  color: Colors.yellow,
                 ),
-                child: Center(child: Text('by')),
-              ),
-              Container(
-                height: 60,
-                width: 150,
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                ),
-                child: Center(child: Text('by')),
               ),
             ],
           ),
