e19c63427d26ad3b525c64cba9f4940957a3ff8a
SialAyesha <wizz.ayesha@gmail.com>
2023-03-22 11:12:26 +0500

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,63 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(
-
+      home: SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+              Container(
+                height: 60,
+                width: 150,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Center(child: Text('by')),
+              ),
+              Container(
+                height: 60,
+                width: 150,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Center(child: Text('by')),
+              ),
+              Container(
+                height: 60,
+                width: 150,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Center(child: Text('by')),
+              ),
+              Container(
+                height: 60,
+                width: 150,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Center(child: Text('by')),
+              ),
+              Container(
+                height: 60,
+                width: 150,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                ),
+                child: Center(child: Text('by')),
+              ),
+            ],
+          ),
         ),
       ),
-
     );
   }
 }
